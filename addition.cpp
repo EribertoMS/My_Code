590b8c771c2ae1f68d2c69b6341a87f7304d7935
EribertoMS <135544627+EribertoMS@users.noreply.github.com>
2025-01-08 11:19:34 -0600

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1;
+	int num2;
+	int sum;
+
+	cout << "Enter the first number: ";
+	cin >> num1;
+
+	cout << "Enter the first number: ";
+	cin >> num2;
+
+	sum = num1 + num2;
+	cout << "The sum is: " << sum << endl;
+
+	return 0;
+}
